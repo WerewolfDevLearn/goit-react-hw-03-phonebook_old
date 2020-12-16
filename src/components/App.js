20d3c92d342ff3c7105744db9116f25de1c9a358
WerewolfDevLearn <58573476+WerewolfDevLearn@users.noreply.github.com>
2020-12-17 00:00:20 +0200

    fix mistakes, add some position rules for button 'add contact'

--- src/components/App.js
+++ src/components/App.js
@@ -25,7 +25,7 @@ export default class App extends Component {
   }
 
   componentDidUpdate(prevState) {
-    if (prevState !== this.state) {
+    if (prevState !== this.state.contacts) {
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
   }
